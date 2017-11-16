d535e69c9e36a7ba1a1e4da69e245dfa99d8da7b
diptadas <dipta@appscode.com>
2017-11-16 16:45:35 +0600

    fix configmap-lock-name

--- pkg/util/kubernetes.go
+++ pkg/util/kubernetes.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/appscode/go/log"
@@ -452,7 +453,7 @@ func ToBeInitializedBySelf(initializers *metav1.Initializers) bool {
 }
 
 func GetConfigmapLockName(workload api.LocalTypedReference) string {
-	return fmt.Sprintf("lock-%s-%s", workload.Kind, workload.Name)
+	return strings.ToLower(fmt.Sprintf("lock-%s-%s", workload.Kind, workload.Name))
 }
 
 func DeleteConfigmapLock(k8sClient kubernetes.Interface, namespace string, workload api.LocalTypedReference) error {
